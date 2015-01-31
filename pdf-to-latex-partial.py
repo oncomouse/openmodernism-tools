54e9842c21d94f8bddadb3d98674f66f2b4e236c
oncomouse <oncomouse@gmail.com>
2015-01-31 10:28:41 -0700

    Converted to a function so we can parse multiple files. Also switched from pdfimages to pdftocairo for the image conversion.

--- pdf-to-latex-partial.py
+++ pdf-to-latex-partial.py
@@ -2,36 +2,50 @@ from subprocess import call
 import glob
 import string
 import os
+import argparse
 
 # Script to convert PDFs w/ scanned page images into LaTeX partials.
 
-target_file = "Mina Loy - History of Religion of Eros.pdf"
+#target_file = "Mina Loy - History of Religion of Eros.pdf"
 
-# This requires poppler to work
-return_status = call(["pdfimages", "-png", target_file, "page-images/page"])
+def process_file( target_file ):
+    
+    # Make the directory:
+    if not os.path.exists(target_file.replace('.pdf','')):
+        os.makedirs(target_file.replace('.pdf',''))
+    if not os.path.exists(target_file.replace('.pdf','') + "/page-images"):
+        os.makedirs(target_file.replace('.pdf','') + "/page-images")
 
-# This version uses ghostscript and imagemagick but doesn't produce as high-quality results:
-#return_status = call(["convert", '-trim', target_file, '-resize 500%','-quality 100','-sharpen 0x1.0',  os.getcwd() + "/page-images/page.png"])
+    # This requires poppler to work:
+    return_status = call(["pdftocairo", "-png", target_file, target_file.replace('.pdf','') + "/page-images/page"])
+    # This version takes forever to run, but it works on a wider range of test files. pdfimages is faster but doesn't always work.
+    
+    # This version uses ghostscript and imagemagick but doesn't produce as high-quality results:
+    #return_status = call(["convert", '-trim', target_file,  os.getcwd() + "/page-images/page.png"])
 
-# Make the directory:
-if not os.path.exists("page-images"):
-    os.makedirs("page-images")
+    # Get a list of all the page numbers:
+    page_images = glob.glob(os.getcwd() + "/" + target_file.replace('.pdf','') + "/page-images/*.png")
 
-# Get a list of all the page numbers:
-page_images = glob.glob(os.getcwd() + '/page-images/*.png')
+    # Base from which we include each image file:
+    base_image_template = "\\begin{{figure}}[hbt]\n\t\\centering\n\t\\includegraphics[height=11in,width=8.5in,keepaspectratio]{{{}}}\n\\end{{figure}}\n\\clearpage\n\n"
 
-# Base from which we include each image file:
-base_image_template = "\\begin{{figure}}[hbt]\n\t\\centering\n\t\\includegraphics[height=11in,width=8.5in,keepaspectratio]{{{}}}\n\\end{{figure}}\n\n"
+    # Open a LaTeX file
+    f = open(target_file.replace(".pdf", ".tex"), "w")
 
-# Open a LaTeX file
-f = open(target_file.replace(".pdf", ".tex"), "w")
+    # Write the geometry and page fraction reset code:
+    f.write("\\newgeometry{{margin=0in}}\n\\providecommand{\\oldpagefraction}{\\floatpagefraction}\n\\renewcommand{\\floatpagefraction}{0.1}\n\n")
 
-# Write the geometry and page fraction reset code:
-f.write("\\newgeometry{{margin=0in}}\n\\providecommand{\\oldpagefraction}{\\floatpagefraction}\n\\renewcommand{\\floatpagefraction}{0.1}\n\n")
+    for page_image in page_images:
+        f.write(base_image_template.format(page_image.replace(".png", "").replace(os.getcwd() + "/","")))
 
-for page_image in page_images:
-    f.write(base_image_template.format(page_image.replace(".png", "").replace(os.getcwd() + "/","")))
+    # Write the geometry and page fraction restore code:
+    f.write("\\restoregeometry\n\\renewcommand{\\floatpagefraction}{\\oldpagefraction}")
+    f.close()
 
-# Write the geometry and page fraction restore code:
-f.write("\\restoregeometry\n\\renewcommand{\\floatpagefraction}{\\oldpagefraction}")
-f.close()
+# Read file names off of the command line and process them:
+argparser = argparse.ArgumentParser()
+argparser.add_argument('files', metavar='FILE', type=str, nargs='*')
+files = argparser.parse_args().files
+
+for file in files:
+    process_file(file)
